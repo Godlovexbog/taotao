a48c641ae1062906235898cb7659664c92fbbea2
wubo <80210534@qq.com>
2018-04-16 17:23:46 +0800

    内容添加通过

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.taotao.content.service.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.content.service.ContentService;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
+
+import com.taotao.pojo.TbContentExample;
+import com.taotao.pojo.TbContentExample.Criteria;
 
 @Service
 public class ContentServiceImpl implements ContentService {
@@ -24,6 +28,38 @@ public class ContentServiceImpl implements ContentService {
 		//插入到内容表
 		contentMapper.insert(content);
 		return TaotaoResult.ok();
+	}
+	
+	@Override
+	public List<TbContent> getContentByCid(long cid) {
+		//先查询缓存
+		//添加缓存不能影响正常业务逻辑
+/*		try {
+			//查询缓存
+			String json = jedisClient.hget(INDEX_CONTENT, cid + "");
+			//查询到结果，把json转换成List返回
+			if (StringUtils.isNotBlank(json)) {
+				List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+				return list;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+		//缓存中没有命中，需要查询数据库
+		TbContentExample example = new TbContentExample();
+		Criteria criteria = example.createCriteria();
+		//设置查询条件
+		criteria.andCategoryIdEqualTo(cid);
+		//执行查询
+		List<TbContent> list = contentMapper.selectByExample(example);
+		//把结果添加到缓存
+/*		try {
+			jedisClient.hset(INDEX_CONTENT, cid + "", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+		//返回结果
+		return list;
 	}
 
 }
