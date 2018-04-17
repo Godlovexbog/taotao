668a0c2072e85c9777b613c75af17c2b479a3f58
wubo <80210534@qq.com>
2018-04-17 16:14:41 +0800

    day05jedis添加缓存

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -3,11 +3,15 @@ package com.taotao.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.content.service.ContentService;
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 
@@ -19,6 +23,11 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Autowired
+	private JedisClient jedisClient;
+	
+	@Value("${INDEX_CONTENT}")
+	private String INDEX_CONTENT;
 	
 	@Override
 	public TaotaoResult addContent(TbContent content) {
@@ -27,14 +36,17 @@ public class ContentServiceImpl implements ContentService {
 		content.setUpdated(new Date());
 		//插入到内容表
 		contentMapper.insert(content);
+		//同步缓存
+		//删除对应的缓存信息
+		jedisClient.hdel(INDEX_CONTENT, content.getCategoryId().toString());
 		return TaotaoResult.ok();
 	}
-	
+
 	@Override
 	public List<TbContent> getContentByCid(long cid) {
 		//先查询缓存
 		//添加缓存不能影响正常业务逻辑
-/*		try {
+		try {
 			//查询缓存
 			String json = jedisClient.hget(INDEX_CONTENT, cid + "");
 			//查询到结果，把json转换成List返回
@@ -44,7 +56,7 @@ public class ContentServiceImpl implements ContentService {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}*/
+		}
 		//缓存中没有命中，需要查询数据库
 		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
@@ -53,13 +65,14 @@ public class ContentServiceImpl implements ContentService {
 		//执行查询
 		List<TbContent> list = contentMapper.selectByExample(example);
 		//把结果添加到缓存
-/*		try {
+		try {
 			jedisClient.hset(INDEX_CONTENT, cid + "", JsonUtils.objectToJson(list));
 		} catch (Exception e) {
 			e.printStackTrace();
-		}*/
+		}
 		//返回结果
 		return list;
 	}
+
 
 }
